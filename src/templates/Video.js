5965c5e64dc64e5518808537df945e755d5604dd
Jayspart45 <jayamstrikerjrk@gmail.com>
2022-09-22 17:24:37 +0530

    s

--- src/templates/Video.js
+++ src/templates/Video.js
@@ -5,8 +5,7 @@ import video from './images/video.mp4';
 export default function Video() {
     return (
         <div className="video">
-            <iframe src={video} autoPlay frameborder="0"></iframe>
-            {/* <video autoPlay loop controls src={video}></video> */}
+            <video autoPlay loop controls src={video}></video>
         </div>
     );
 }
